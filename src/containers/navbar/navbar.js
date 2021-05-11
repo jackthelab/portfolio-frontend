3ba9f0781ba28b83702a0b857e527795df07684c
Jacob Hampton <jhampton792@gmail.com>
2021-05-11 09:22:03 -0500

    Changed color scheme

--- src/containers/navbar/navbar.js
+++ src/containers/navbar/navbar.js
@@ -1,12 +1,13 @@
 import './navbar.css';
-import logo from './resources/logo.png'
+import logo from './resources/logo.png';
+import logoTwo from './resources/logo-2.png';
 
 const NavBar = () => {
 
     return (
         <section id="nav-bar">
             <div className="nav">
-                <img className="logo" src={ logo } alt="Jacob H. Development Logo" />
+                <img className="logo" src={ logoTwo } alt="Jacob H. Development Logo" />
                 <div className="nav-options-list">
                     <h2 className="nav-option"><a href="#about">About</a></h2>
                     <h2 className="nav-option"><a href="#projects">Projects</a></h2>
